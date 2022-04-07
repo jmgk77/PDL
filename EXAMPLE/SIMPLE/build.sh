6006b240c9e734fd996faba99f9aa896aa13480e
JMGK <jmgk77@gmail.com>
2022-04-06 22:10:45 -0300

    example 1

--- EXAMPLE/SIMPLE/build.sh
+++ EXAMPLE/SIMPLE/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm target.dll malware.dll main.exe
+rm *.dll *.exe
 x86_64-w64-mingw32-gcc -o target.dll -shared target.c target.def -Wl,--subsystem,windows -s
 x86_64-w64-mingw32-gcc -o malware.dll -shared malware.c -Wl,--subsystem,windows -s
 x86_64-w64-mingw32-gcc -o main.exe main.c -L. -ltarget -s
--- EXAMPLE/SIMPLE/main.c
+++ EXAMPLE/SIMPLE/main.c
@@ -7,10 +7,12 @@ __declspec(dllimport) int __cdecl functionC(int a, int b, int c);
 __declspec(dllimport) int __cdecl functionD(int a, int b, int c, int d);
 
 int main(int argc, char **argv) {
+    printf("* [MAIN.EXE] INIT\n");
   if (functionA() + functionB(0, 0) + functionC(0, 0, 0) +
           functionD(0, 0, 0, 0) ==
       0) {
-    printf("* [MAIN.EXE]\n");
+    printf("* [MAIN.EXE] OK\n");
   }
+    printf("* [MAIN.EXE] DONE\n");
   return EXIT_SUCCESS;
 }
